7f944c2f447196ab234f4dea5fcb6c460064a16b
YuriLeocadio <yurileocadio0411@gmail.com>
2026-08-18 00:13:23 -0300

    test: atualizar main para carregar dados via arquivo

--- src/main/java/com/yurileocadio/bookflow/Main.java
+++ src/main/java/com/yurileocadio/bookflow/Main.java
@@ -1,33 +1,34 @@
 package com.yurileocadio.bookflow;
 
-import com.yurileocadio.bookflow.dominio.Aluno;
+import com.yurileocadio.bookflow.arquivo.CargaDadosIniciais;
 import com.yurileocadio.bookflow.dominio.Emprestimo;
 import com.yurileocadio.bookflow.dominio.Livro;
+import com.yurileocadio.bookflow.dominio.Usuario;
 import com.yurileocadio.bookflow.exception.LimiteEmprestimosException;
 import com.yurileocadio.bookflow.exception.LivroIndisponivelException;
 
 public class Main {
     public static void main(String[] args) {
-        Aluno joao = new Aluno("João", 20, "12345678900", "Rua A");
-        Livro livro = new Livro("Dom Casmurro", "Machado de Assis", "Romance");
+        Livro[] livros = CargaDadosIniciais.carregarLivros();
+        Usuario[] usuarios = CargaDadosIniciais.carregarUsuarios();
 
-        try{
-            Emprestimo e1 = Emprestimo.criar(joao, livro);
+        try {
+            Emprestimo e1 = Emprestimo.criar(usuarios[0], livros[0]);
             System.out.println("Emprestimo criado");
-        }catch (LivroIndisponivelException e){
+        } catch (LivroIndisponivelException e) {
             e.printStackTrace();
             System.out.println("BUG: não deveria falhar aqui - " + e.getMessage());
-        } catch (LimiteEmprestimosException e){
+        } catch (LimiteEmprestimosException e) {
             e.printStackTrace();
             System.out.println("BUG: não deveria falhar aqui - " + e.getMessage());
         }
 
-        try{
-            Emprestimo e2 = Emprestimo.criar(joao, livro);
+        try {
+            Emprestimo e2 = Emprestimo.criar(usuarios[0], livros[0]);
             System.out.println("Emprestimo criado");
-        }catch (LivroIndisponivelException e){
+        } catch (LivroIndisponivelException e) {
             System.out.println(e.getMessage());
-        } catch (LimiteEmprestimosException e){
+        } catch (LimiteEmprestimosException e) {
             e.printStackTrace();
             System.out.println("BUG: não deveria falhar aqui - " + e.getMessage());
         }
